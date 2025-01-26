7dadd0095891b332939761ff953b6946a251f3d9
Wachid Susilo <wachidsusilo@gmail.com>
2025-01-27 04:37:34 +0700

    chore: remove DisplacementUnitInfo, already replaced by PositionUnitInfo

--- Moxion/Domain/Units/DisplacementUnitInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using Moxion.Abstractions;
-using Moxion.Common.Units;
-
-namespace Moxion.Domain.Units;
-
-public readonly record struct DisplacementUnitInfo( PositionUnit Unit ) : IUnitInfo<DisplacementUnitInfo>;
